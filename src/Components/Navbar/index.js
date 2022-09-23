e18a5b55c80f1af7ec96c06d99960dd7236d07ae
Inzamam <inzamam@credencesoft.in>
2022-09-23 20:09:54 +0530

    Added responsiveness to first 3 sections and Navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Logo from '../../assets/images/logo.png';
+import {IoMenu} from 'react-icons/io5';
+import {ImCross} from 'react-icons/im';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className='navbar-container'>
       <div className="navbar-logo">
@@ -15,7 +18,22 @@ const Navbar = () => {
         <span className="navbar-options">Projects</span>
         <span className="navbar-options">Clients</span>
       </div>
+      <div className="navbar-side-buttons">
+      <a className='navbar-menu-dropdown-container'>
+        {!showMenu && <IoMenu onClick={()=>setShowMenu(true)} className='navbar-menu-button' size={35} />}
+        {showMenu && <ImCross onClick={()=>setShowMenu(false)} className='navbar-menu-button' size={25} />}
+        {showMenu && 
+        <ul className="navbar-menu-dropdown">
+        <li>About Us</li>
+        <li>Our Team</li>
+        <li>What we do</li>
+        <li>Projects</li>
+        <li>Clients</li>
+        <li>Contact Us</li>
+        </ul>}
+      </a>
       <span className="navbar-contact-button">Contact Us</span>
+      </div>
     </div>
   )
 }
